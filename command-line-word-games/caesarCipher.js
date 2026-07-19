2cf85637d6cfa4f23c06bf9f9a0fe80d60a58962
Lera Chuiko <lera.chuiko@gmail.com>
2026-07-19 20:01:26 +0200

    fix

--- command-line-word-games/caesarCipher.js
+++ command-line-word-games/caesarCipher.js
@@ -2,14 +2,14 @@
 //const satz = process.argv[2];
 //const shiftInput = process.argv[3];
 
-const satz = "";
-const shiftInput = '';
+const satz = "hello world";
+const input = -33;
 
-const shift = parseInt(shiftInput, 10); // Сюда поставили shiftInput
+const shift = Number(input);
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
-let encryptedsatz = "";
+let caesarSatz = "";//Um etwas zu füllen, benötigen wir eine leere Container
 
-//-----------------1------------------//
+//-----------------1------------------// Ich führe eine Überprüfung durch
 
 if (!satz || !shift|| isNaN(shift)) {
   console.log("Fehler: Bitte geben Sie die Satz und die Verschiebung");
@@ -17,20 +17,48 @@ if (!satz || !shift|| isNaN(shift)) {
 }
 
 
-const lowerCasesatz = satz.toLowerCase();
+const lowerCaseSatz = satz.toLowerCase();
 //-----------------2------------------//
 
-for (let i = 0; i < lowerCasesatz.length; i++) {
-  const currentLetter = lowerCasesatz[i];
+for (let i = 0; i < lowerCaseSatz.length; i++) {
+  
+  const currentLetter = lowerCaseSatz[i];
+
   const currentIndex = alphabet.indexOf(currentLetter);
 
   if (currentIndex === -1) {
-    encryptedsatz += currentLetter;
+    caesarSatz += currentLetter;
   } else {
-    let newIndex = (currentIndex + (shift % 26) + 26) % 26;
-    encryptedsatz += alphabet[newIndex];
+    let newIndex = (currentIndex + (shift % 26) + 26) % 26; //operationen
+    caesarSatz += alphabet[newIndex];
   }
 }
 //-----------------3------------------//
 
-console.log(encryptedsatz);
+console.log(caesarSatz);
+
+//---------------Fazit-----------------//
+
+// Diese Aufgaben fördern abstraktes Denken:
+// *Wir können eine Linie in einen Ring umwandeln.
+// *Abwägung von Vor- und Nachteilen, um zu verhindern, 
+// dass ein Programm seine Grenzen überschreitet.
+
+
+// Beispiele:
+// Circular Buffer- Dieser Speicher wird kreisförmig beschrieben: 
+// Sobald er das Ende erreicht, werden neue Daten ganz an den Anfang 
+// geschrieben und überschreiben die alten. Dadurch können Programme 
+// unbegrenzt laufen, ohne den Arbeitsspeicher (RAM) des Computers zu überlasten.
+
+// Kryptografie
+
+// Waffenwechsel: Der Spieler drückt die Q-Taste, um zwischen den Waffen 
+// zu wechseln (Maschinengewehr, Pistole, Messer, wieder Maschinengewehr). 
+// Die Logik von „nächstes/vorheriges“ ist ein grundlegender Algorithmus für Spiele.
+
+// Kartenbewegung: Denken Sie an klassische Spiele wie Pac-Man oder Snake.
+
+// Fotogalerie oder Banner-Karussell
+
+// Widgets zur Zeitauswahl
